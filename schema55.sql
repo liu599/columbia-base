7cae63c437a2f91dd896500f37ff6c7b06db42b3
liu599 <970228409@qq.com>
2026-02-07 16:51:40 +0800

    feat(deployment): finish deployment

---
+++ schema55.sql
@@ -0,0 +1,102 @@
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user`  (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `username` varchar(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '哈希密码',
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '手机号',
+  `wechat_openid` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '微信唯一标识',
+  `status` int(11) NULL DEFAULT NULL COMMENT '0: 待激活, 1: 正常, 2: 锁定',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '注册时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
+
+-- ----------------------------
+-- Table structure for t_user_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user_auth`;
+CREATE TABLE `t_user_auth`  (
+  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '关联用户',
+  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '长令牌',
+  `login_ip` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '登录 IP，安全审计用',
+  `expire_time` datetime NULL DEFAULT NULL COMMENT '过期时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
+
+-- ----------------------------
+-- Table structure for t_product
+-- ----------------------------
+DROP TABLE IF EXISTS `t_product`;
+CREATE TABLE `t_product`  (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '产品名，如：高中数学审阅 Agent',
+  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL COMMENT '描述',
+  `base_credits` int(11) NULL DEFAULT NULL COMMENT '激活该产品赠送的初始积分，可选',
+  `status` int(11) NULL DEFAULT 1 COMMENT '1: 启用, 0: 下架/隐藏',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
+
+-- ----------------------------
+-- Table structure for t_credit_balance
+-- ----------------------------
+DROP TABLE IF EXISTS `t_credit_balance`;
+CREATE TABLE `t_credit_balance`  (
+  `user_id` bigint(20) NOT NULL COMMENT '主键/唯一',
+  `available_credits` int(11) NULL DEFAULT NULL COMMENT '可用积分',
+  `frozen_credits` int(11) NULL DEFAULT NULL COMMENT '冻结积分，用于 Agent 任务运行中扣除',
+  `update_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
+
+-- ----------------------------
+-- Table structure for t_credit_log
+-- ----------------------------
+DROP TABLE IF EXISTS `t_credit_log`;
+CREATE TABLE `t_credit_log`  (
+  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NULL DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT 'RECHARGE: 充值, ACTIVATE: 激活奖励, CONSUME: 消费, FREEZE: 冻结, UNFREEZE: 解冻',
+  `amount` int(11) NULL DEFAULT NULL COMMENT '变动金额，正负值',
+  `task_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '关联 Python 侧的任务 ID，方便对账',
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
+
+-- ----------------------------
+-- Table structure for t_activation_code
+-- ----------------------------
+DROP TABLE IF EXISTS `t_activation_code`;
+CREATE TABLE `t_activation_code`  (
+  `id` bigint(20) NOT NULL,
+  `code` varchar(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '唯一索引，建议 8-12 位随机字符',
+  `product_id` bigint(20) NULL DEFAULT NULL COMMENT '关联产品 ID',
+  `status` int(11) NULL DEFAULT NULL COMMENT '0: 未使用, 1: 已使用, 2: 已过期',
+  `user_id` bigint(20) NULL DEFAULT NULL COMMENT '谁核销了这个码',
+  `used_time` datetime NULL DEFAULT NULL COMMENT '核销时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_code`(`code`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
+
+-- ----------------------------
+-- Table structure for t_audit_log
+-- ----------------------------
+DROP TABLE IF EXISTS `t_audit_log`;
+CREATE TABLE `t_audit_log`  (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `admin_id` bigint(20) NOT NULL COMMENT '操作者 ID，即管理员 ID',
+  `module` varchar(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '所属模块 (USER, PRODUCT, CREDIT, ACTIVATION)',
+  `action` varchar(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '具体动作',
+  `target_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '操作对象 ID (user_id 或 product_id)',
+  `before_value` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL COMMENT '操作前的原始数据',
+  `after_value` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL COMMENT '操作后的新数据/请求 Body',
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '操作备注',
+  `ip_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL COMMENT '操作员 IP 地址',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '操作时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_admin_id`(`admin_id`) USING BTREE,
+  INDEX `idx_module_action`(`module`, `action`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci;
