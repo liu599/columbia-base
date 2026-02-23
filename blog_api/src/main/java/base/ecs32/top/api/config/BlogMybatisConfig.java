fa622d9144b661f418beb946b051ae20f48d2d36
liu599 <970228409@qq.com>
2026-02-23 22:23:59 +0800

    fix(blog): add missing DataSourceTransactionManager import

--- blog_api/src/main/java/base/ecs32/top/api/config/BlogMybatisConfig.java
+++ blog_api/src/main/java/base/ecs32/top/api/config/BlogMybatisConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
